3f44b9c9fc1a5280ec7d995a491e72164eeb4f82
Hassan Bari Raza <hbr8218@gmail.com>
2020-03-12 11:43:37 +0530

    just imported flask-restplus and configured it

--- myPackage/__init__.py
+++ myPackage/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_restplus import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 
 
 # Instantiating Flask
 app = Flask(__name__)
+app = Api(app)
 # Configuration of flask object
 app.config['SECRET_KEY']='57333b23060b97386c2ed279e796e6fe'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysqL2016@localhost/incubator'
