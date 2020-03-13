a820ae028d4b38417e3fbe9069a430e88e0984b2
Hassan Bari Raza <hbr8218@gmail.com>
2020-03-13 11:24:52 +0530

    hide database user and password

--- myPackage/__init__.py
+++ myPackage/__init__.py
@@ -1,12 +1,16 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+# Getting user and password for database connection
+db_user = os.environ.get('DB_USER')
+db_pass = os.environ.get('DB_PASSWORD')
 
 # Instantiating Flask
 app = Flask(__name__)
 # Configuration of flask object
 app.config['SECRET_KEY']='57333b23060b97386c2ed279e796e6fe'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysqL2016@localhost/incubator'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://'+db_user+':'+db_pass+'@localhost/incubator'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 db = SQLAlchemy(app)
